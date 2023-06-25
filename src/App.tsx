4025a964ac1c02d00af06b0f7d29037e76627e0f
olefalcon <olefalcon@gmail.com>
2023-06-25 18:36:47 -0400

    Hash router changes for gh pages
    Change browser router to hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import {Main} from './pages/main/main';
 import {Register} from './pages/register';
